3c2193b3f720b999a7102b359c9e3b33462d6a4d
A-Vojislav <115400420+A-Vojislav@users.noreply.github.com>
2022-11-26 11:00:56 +0100

    prvi set zadataka

--- Novembar/subota 26.11/main.js
+++ Novembar/subota 26.11/main.js
@@ -7,21 +7,91 @@
 // Output: false
 
 
-function task1(input){
-    let splitInput=input.split('');
-    let isNumberThere=false;
+let task1=function (input){
+    let splitInput = input.split('');
+    let isNumberThere = false;
     let counter=0;
-            for(i=0;i<input.length;i++){
+            for(let i=0;i<input.length;i++){
+              
                 if(Number.isInteger(parseInt(splitInput[i]))){
-                            counter++
+                            counter++;
+                            if(splitInput[i]==5 || splitInput[i]=='5' ){
+                                console.log(`there is a string or Number 5 in the Input`);
+                                break;
+                            } else {
+                                console.log(`there is no number or string with '5`)
+                                break;
+                            }
                         }
             }
-            console.log(counter)
+
+
         if (counter==5){
-            return true
+            return true;
         } else{
-            return false
+            return false;
         }
+
+
 }
 
-task1('1b8952abd')
+task1('1b8952abd6');
+
+
+// 2. Write a function that in a given string replaces all the appearances of the string ‘JS’ with ‘**’. 
+// Input: “Programming in JS is super interesting!”
+// Output: “Programming in ** is super interesting!”
+
+function task2 (input) {
+    return input.replaceAll('JS', '**');
+}
+console.log(task2('Programming in JS is super interesting!'));
+
+
+// 3. Write a function that inserts a given character on a given position in the string.
+// Input: “Goo morning”, 4, ‘d’ 
+// Output: “Good morning” 
+
+
+let task3=function (index,string){
+    let text='Goo morning';
+    let textarr=Array.from(text)
+    textarr.splice(index-1,0,string)
+    return textarr.join('')
+}
+
+console.log(task3(4,'d'))
+
+// 4. Write a function that deletes a character from the given position in the string. 
+// Input: “Goodd morning!”, 3 
+// Output: “Good morning!” 
+
+let task4=function (inputString,index){
+
+    let outputString=inputString.split('');
+
+    outputString.splice(index,1);
+
+    return outputString.join('');
+}
+
+ console.log(task4('Goodd morning!',3))
+
+
+//  5. Write a function that deletes every second element of the given array.
+// Input: [3, 5, 1, 8, 90, -4, 23, 1, 67]
+// Output: [3, 1, 90, 23, 67]
+
+let task5= function(input){
+    let output=[];
+    for(let i=0; i<input.length;i++) {
+        if(i%2==0){
+            output.push(input[i]);
+        }
+    }
+    return output
+    
+}
+console.log(task5([3, 5, 1, 8, 90, -4, 23, 1, 67]));
+
+
