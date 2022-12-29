9f41978eb78b5da32b7f8276ccb95f0b9a1a7119
A-Vojislav <115400420+A-Vojislav@users.noreply.github.com>
2022-12-29 20:40:24 +0100

    updATE

---
+++ Decembar/cetvrtak-29-12/main.js
@@ -0,0 +1,48 @@
+//task1
+
+let colors = ["Orangered","Blue","Yellow","Green","Orange", "Cyan","DodgerBlue", "Violet"];
+let index = 0;
+const btnBasic = document.getElementById('button1');
+const backgroundBasic= document.getElementById('basicBackground');
+
+function changeColor(){
+    if(index > colors.length - 1)
+    index = 0;
+  backgroundBasic.style.backgroundColor = colors[index++]; 
+}
+
+btnBasic.addEventListener('click', changeColor);
+
+// task2
+
+const btnAutoChangeColor= document.getElementById('button2');
+
+
+function addAutoChangeColor(){
+    const autoChangeBackground=document.getElementById('advance')
+    autoChangeBackground.classList.add('advance');
+}
+function cancelAutoChangeColor(){
+    const autoChangeBackground=document.getElementById('advance')
+    autoChangeBackground.classList.remove('advance');
+}
+
+document.getElementById('button2').addEventListener('click', addAutoChangeColor);
+document.getElementById('button3').addEventListener('click', cancelAutoChangeColor);
+
+// task3
+
+
+const chatRoom= document.getElementById('chatRoom');
+const chatRoomOutput=document.getElementById('output')
+const addMessageChatRoomButton= document.getElementById('submit')
+const inputText=document.getElementById('input')
+let outputMemory=[];
+function addMessageHandler (){
+    chatRoomOutput.innerHTML+=`
+    <p>${inputText.value}</p>
+    `
+
+}
+
+addMessageChatRoomButton.addEventListener('click', addMessageHandler)
